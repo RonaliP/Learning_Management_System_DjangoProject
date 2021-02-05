826852c8e08310d776335f84a1d3d23a5a29d3e0
Ronali <ronalipanigrahy88@gmail.com>
2021-02-06 01:40:06 +0530

    urls added for both apps

--- Learning_Management_System/urls.py
+++ Learning_Management_System/urls.py
@@ -22,7 +22,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="LMS API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
