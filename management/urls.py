c4503acb794cd5247d149ada66b08eb5f7932ef0
Ronali <ronalipanigrahy88@gmail.com>
2021-02-06 01:44:00 +0530

    urls added

--- management/urls.py
+++ management/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from management.views import UpdateStudentDetails, UpdateEducationDetails
 
 urlpatterns = [
-    path('update-details/', UpdateStudentDetails.as_view(), name='update-details'),
-    path('update-edu-details/', UpdateEducationDetails.as_view(), name='update-edu-details'),
+    path('update-personal-details/', UpdateStudentDetails.as_view(), name='update-details'),
+    path('update-education-details/', UpdateEducationDetails.as_view(), name='update-edu-details'),
 ]
