010b58dad737c3600eb29fa1c6749f8dcb7a6b26
Ronali <ronalipanigrahy88@gmail.com>
2021-02-06 11:13:47 +0530

    refactored to add mentor permission

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -3,3 +3,10 @@ from rest_framework import permissions
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == 'Admin'
+
+class IsMentor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.role == 'Admin' or request.user.role == 'Mentor':
+            return True
+        else:
+            return False
