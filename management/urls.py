c85ea0a0b06c52db17dfff1a5b39ef15b2226b3c
Ronali <ronalipanigrahy88@gmail.com>
2021-02-06 11:29:28 +0530

    id field added to update student details

--- management/urls.py
+++ management/urls.py
@@ -4,8 +4,8 @@ from management.views import UpdateStudentDetails, UpdateEducationDetails,Course
 
 
 urlpatterns = [
-    path('update-personal-details/', UpdateStudentDetails.as_view(), name='update-details'),
-    path('update-education-details/', UpdateEducationDetails.as_view(), name='update-edu-details'),
+    path('update-personal-details/<int:id>', UpdateStudentDetails.as_view(), name='update-details'),
+    path('update-education-details/<int:id>', UpdateEducationDetails.as_view(), name='update-edu-details'),
     path('courses/', Courses.as_view(), name='courses'),
     path('course/<int:id>', CourseDetails.as_view(), name='course'),
     path('mentors/', Mentors.as_view(), name='mentors'),
